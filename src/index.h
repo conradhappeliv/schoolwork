4509806d19528ad6486634278768c8e31c52a6f8
conradhappeliv <conrad.h.appel.iv@gmail.com>
2014-11-15 15:39:08 -0600

    PROJ5: index interface

--- src/index.h
+++ src/index.h
@@ -2,14 +2,33 @@
 #define INDEX_H
 
 #include <string>
+#include <vector>
 
-// abstraction layer on top of either AVL tree or hash table
+// abstract interface for index
 class Index
 {
 public:
-    virtual void addEntry(std::string) = 0;
-    virtual void saveToFile(std::string) = 0;
-    
+    Index(std::string filen):filename(filen) {}
+    virtual void add(std::string, unsigned int, std::string) = 0;
+    virtual void save() = 0;
+    virtual void load() = 0;
+    virtual void clear() = 0;
+
+    // we can use a hashtable for each of these and actually implement them in this parent class
+    std::string IDtoTitle(unsigned int);
+    std::string IDtoText(unsigned int);
+protected:
+    std::string filename;
+    struct entry {
+        struct doc {
+            unsigned int id;
+            double termFreq;
+        };
+        std::string keyword;
+        std::vector<doc> documents;
+        double idf;
+    };
 };
 
 #endif // INDEX_H
+
