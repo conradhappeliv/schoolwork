f5fdc8c01e3906c8f9ebb9d7d1d1ee6dd18bf964
edjli2005 <edjli2005@gmail.com>
2014-12-06 22:10:10 -0600

    recommit deleted files

--- src/avltreeindex.h
+++ src/avltreeindex.h
@@ -2,34 +2,38 @@
 
 #ifndef AVLTREEINDEX_H
 #define AVLTREEINDEX_H
+
 #include <string>
+#include "index.h"
 
 const int biasleft = -1;
 const int balanced = 0;
 const int biasright = 1;
+enum printPath {LEFT, KEY, RIGHT};
 
-struct AVLTreeNode
-{
+struct AVLTreeNode : public Index {
     std::string key;
-    // struct entry;
-    AVLTreeNode *left;
-    AVLTreeNode *right;
-    AVLTreeNode *parent;
+    entry nodeentry;
+    AVLTreeNode* left;
+    AVLTreeNode* right;
+    AVLTreeNode* parent;
     int balance;
 };
 
-class AVLTreeIndex
+class AVLTreeIndex : public Index
 {
-private:
-    AVLTreeNode *root;
-    void clear(AVLTreeNode *n);
-    void print(AVLTreeNode *n);
-    AVLTreeNode* find(std::string keyword); // TODO: to be implemented
-
 public:
-    AVLTreeIndex();
+    AVLTreeIndex(std::string);
     ~AVLTreeIndex();
+    virtual void add(const unsigned int, const std::string, const unsigned int);
+    virtual void save();
+    virtual void load();
+    virtual void clear();
+    virtual void find(std::string);
+    virtual std::map<unsigned int, unsigned int> findAll(std::string);
 
+private:
+    AVLTreeNode* search(std::string compkey);
     void insert(AVLTreeNode *n);
     void restoreBalance(AVLTreeNode *parent, AVLTreeNode *newNode);
     void adjustBalance(AVLTreeNode *last, AVLTreeNode *first);
@@ -37,7 +41,13 @@ public:
     void rotateRight(AVLTreeNode *n);
     void adjustLR(AVLTreeNode *last, AVLTreeNode *first);
     void adjustRL(AVLTreeNode *last, AVLTreeNode *first);
-    void printTree();
+
+    void dumpTree(std::ofstream& fout, const AVLTreeNode* node);
+    void print(std::ofstream& fout, printPath dir, const AVLTreeNode* node);
+
+    AVLTreeNode *root;
+    void purge(AVLTreeNode *n);
+    void addEntry (const entry e);
 };
 
 #endif // AVLTREEINDEX_H
