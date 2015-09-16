072b454f5b8dcc64a71663fcca1693989e3cbf03
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-09-16 18:34:10 -0500

    refactor aLL the tHiNGS

--- conrad.m
+++ conrad.m
@@ -1,11 +1,7 @@
 SampleFreq = 8000;
 t = 0:1/SampleFreq:1;
 
-% MESSAGE
-x = sin(2*pi*40*t);
-%x = sin(2*pi*40*t) .* sin(2*pi*80*t) .* sin(2*pi*440*t) .* sin(2*pi*880*t) .* sin(2*pi*4000*t);
-%x = square(t*40);
-%x = sin(40*t) .* cos(60*t);
+x = Message(t);
 
 % PLOT "BEFORE"
 figure(1);
@@ -13,18 +9,9 @@ plot(t, x);
 figure(2);
 PlotFreq(SampleFreq, x);
 
-% MIXER
-x = x .* cos(2*pi*3000*t);
-
-% CHANNEL
-for i=1:size(x),
-   x(i) = x(i) +(-1 + (1+1)*rand(1));
-end
-
-% RECEIVER
-windowSize = round(SampleFreq/150);
-x = x ./ cos(2*pi*3000*t);
-x = filter((1/windowSize)*ones(1,windowSize), 1, x);
+x = Mixer(x, t);
+x = Channel(x);
+x = Receiver(x, t);
 
 % PLOT "AFTER"
 figure(3);
