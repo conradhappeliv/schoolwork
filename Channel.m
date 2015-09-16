cdb173911bee6689594f7e8b36fd37d0a7fbb34f
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-09-16 18:33:17 -0500

    Create Channel.m

---
+++ Channel.m
@@ -0,0 +1,7 @@
+function [ muddled ] = Channel( message )
+    muddled = message;
+    %muddled = zeros(size(message));
+    %for i=1:size(message),
+    %   muddled(i) = message(i) +(-1 + (1+1)*rand(1));
+    %end
+end
