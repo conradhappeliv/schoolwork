a3102803f2bfc4a27692f151edaa226fab476a05
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-09-30 19:23:33 -0500

    Anti-attenuation in the receiver

--- Receiver.m
+++ Receiver.m
@@ -1,4 +1,7 @@
 function [extracted] = Receiver(message, t)
+    %anti-attenuation
+    message = message*10^6;
+    
     extracted = message .* cos(2*pi*3000*t);
     df = designfilt('lowpassfir','PassbandFrequency',1500,...
                 'StopbandFrequency',1900,'PassbandRipple',1,...
