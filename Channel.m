f38ff46547c049652e71e69eca48ab8ba2ec3cdf
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-09-30 19:23:52 -0500

    Attenuation in channel

--- Channel.m
+++ Channel.m
@@ -1,7 +1,6 @@
 function [ muddled ] = Channel( message )
     muddled = message;
-    %muddled = zeros(size(message));
-    %for i=1:size(message),
-    %   muddled(i) = message(i) +(-1 + (1+1)*rand(1));
-    %end
+    
+    % Attenuation
+    muddled = muddled*10^-6;
 end
