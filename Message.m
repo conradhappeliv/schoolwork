c73a151f0a2722035d150107ac0c45c6fbf330c3
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-09-30 19:25:24 -0500

    Cleaner messages and a generic format

--- Message.m
+++ Message.m
@@ -1,25 +1,22 @@
-function [ m ] = Message()
-
+function [ m, t, Fs ] = Message()
     [m, Fs] = audioread('name.m4a');
-    m = m(:,1);
-    Length= length(m);
-    t = (0: Length-1)/Fs;
+    m = m(:,1)';
+    t = (0:length(m)-1)/Fs;
 
-    m = sin(2*pi*40*t);
-
-    %message = sin(2*pi*40*t);
-    %x = sin(2*pi*40*t) .* sin(2*pi*80*t) .* sin(2*pi*440*t) .* sin(2*pi*880*t) .* sin(2*pi*4000*t);
-    %x = square(t*40);
-    %x = sin(40*t) .* cos(60*t);
+    % SIN
+    %Fs = 8000;
+    %t = 0:1/Fs:1;
+    %m = sin(2*pi*40*t);
     
-
+    % SQUARE
+    %Fs = 8000;
+    %t = 0:1/Fs:1;
+    %m = square(t*40);
     
-    m = transpose(m);
-    plot(t,m);
-    m = transpose(m);
-    M= fftshift(fft(m,Length));
-    M_mag= abs(M);
-    df=(-Length/2:Length/2-1)*Fs/Length
-    plot(df, M_mag);
+    % PULSE TRAIN
+    % D = % delay
+    % W = % pulse width
+    % m = pulstran(t,d,'rectpuls', W);
+    
 
 end
