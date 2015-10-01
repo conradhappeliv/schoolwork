4d564aeb6e1342f4aa291ac455dfaf4689955a49
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-09-30 19:23:07 -0500

    Added capability to plot longer signals
    Those with a sample frequency less than the total size of the sample.
    
    Plots to different plots per period of time

--- PlotFreq.m
+++ PlotFreq.m
@@ -1,6 +1,20 @@
 function [] = PlotFreq( sample_freq, x )
     FreqX = fftshift(fft(x));
     FreqX = abs(FreqX);
-    df = -sample_freq/2:1:sample_freq/2;
-    plot(df, FreqX);
+    if(length(x) > sample_freq)
+        endofsamp = 1;
+        while(endofsamp < length(x)-1)
+            beginofsamp = endofsamp;
+            endofsamp = endofsamp + min(length(x)-endofsamp, sample_freq)-1;
+            fragment = FreqX(beginofsamp:endofsamp);
+            df = -length(fragment)/2:1:length(fragment)/2;
+            df = df(1:length(df)-1);
+            figure();
+            plot(df, fragment);
+            title(strcat(num2str(beginofsamp),' to  ',num2str(endofsamp)));
+        end
+    else
+        df = -sample_freq/2:1:sample_freq/2;
+        plot(df, FreqX);
+    end
 end
