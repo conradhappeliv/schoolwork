723c81895f5333cf3287da9b7607659b00ba657d
Conrad H. Appel, IV <me@conradhappeliv.com>
2015-10-28 19:20:55 -0500

    Attenuation, unattentuation, and channel fading

--- channel.py
+++ channel.py
@@ -1,2 +1,22 @@
+import numpy as np
+import random
+
+
 def attenuation(m):
+    m.msg = np.multiply(m.msg, 10**-6)
+    return m
+
+
+def fading(m):
+    str1 = random.uniform(.5, 1.)
+    str2 = random.uniform(0., 1.-str1)
+    str3 = 1. - str1 - str2
+    delay2 = random.randint(1, m.fs)
+    delay3 = random.randint(1, m.fs)
+    for i in range(len(m.msg)):
+        m.msg[i] = m.msg[i] * str1
+        if i-delay2 > 0:
+            m.msg[i] += m.msg[i-delay2]*str2
+        if i-delay3 > 0:
+            m.msg[i] += m.msg[i-delay3]*str3
     return m
--- receiver.py
+++ receiver.py
@@ -1,10 +1,14 @@
 from scipy import signal
+import numpy as np
+
 
 def unattenuate(m):
+    m.msg = np.multiply(m.msg, 10**6)
     return m
 
+
 def lpf(m):
-    filt = signal.firwin(numtaps=10, cutoff=800, nyq=80000)
+    filt = signal.firwin(numtaps=10, cutoff=2000, nyq=100000)
     m.msg = signal.lfilter(filt, 1.0, m.msg)
     return m
 
