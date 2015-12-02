Actually correct scaling in the filter response plot

LPF_response.py
@@ -9,13 +9,13 @@ def main():
     norm_pass = cutoff/(44100/2)
     (N, Wn) = signal.buttord(wp=norm_pass, ws=1.5*norm_pass, gpass=2, gstop=50, analog=0)
     (b, a) = signal.butter(N, Wn, btype='lowpass', analog=0, output='ba')
-    print("b="+str(b)+", a="+str(a))
-
 
     w, h = signal.freqz(b, a)
     plot.figure()
     plot.title("Digital Filter Frequency Response")
-    plot.plot(w/np.pi*44100, 20 * np.log10(abs(h)))
+    plot.plot(w/2/np.pi*44100, 20 * np.log10(abs(h)))
+    plot.xlabel("Frequency (Hz)")
+    plot.ylabel("Gain (dB)")
     plot.show()
 
 