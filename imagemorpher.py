2a7735ca9fc4185f557660433d91ef9d42ac7ca1
Conrad Appel <conrad.h.appel.iv@gmail.com>
2017-04-26 10:39:04 -0500

    use matplotlib instead of dlib window

--- imagemorpher.py
+++ imagemorpher.py
@@ -1,20 +1,19 @@
 import cv2
 import dlib
 from skimage import io
+import matplotlib.pyplot as plt
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('data/shape_predictor_68_face_landmarks.dat')  # get from http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-win = dlib.image_window()
 
 face = io.imread('./data/face1.jpg')
 
 gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 rects = detector(gray, 0)
+plt.imshow(face)
 for k, d in enumerate(rects):
-    win.clear_overlay()
-    win.set_image(face)
     shape = predictor(gray, d)
-    win.add_overlay(shape)
-win.add_overlay(rects)
-dlib.hit_enter_to_continue()
+    for point in shape.parts():
+        plt.plot(point.x, point.y, 'o')
 
+plt.show()
