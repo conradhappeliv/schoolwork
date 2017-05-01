ad7afa616aa5e415878ff72d6248289be52d3b30
locamp <alcampbell703@gmail.com>
2017-04-30 21:48:02 -0500

    gif.py change

--- gif.py
+++ gif.py
@@ -1,5 +1,6 @@
 from images2gif import writeGif
 from PIL import Image  #not sure if we need this one
 
-def make_gif():
+#filename is output file, images is an array
+def make_gif(filename, images):
 writeGif(filename, images, duration=0.2)
